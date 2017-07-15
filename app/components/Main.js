4f2bc87f9d1fcb35faff2b8bea240aeae90eb277
Jimel Joseph <jimelj@gmail.com>
2017-07-15 12:09:27 -0400

    setting up backend and initial commit

---
+++ app/components/Main.js
@@ -0,0 +1,20 @@
+// Include React
+import React from "react"
+
+// Creating the Main component
+class Main extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  // Render Page
+  render() {
+    return (
+      <h1>i am here</h1>
+    );
+  }
+}
+
+// Export the component back for use in other files
+export default Main;
